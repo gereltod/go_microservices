e57022b92fe6286304480b82f8ef75605e884248
gereltod <gereltod.s@gmail.com>
2020-05-27 14:37:00 +0100

    Adding benchmarks

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,36 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	//Initialization:
+
+
+	//Execution:
+	user, err := GetUser(0)
+
+	//Validation:
+	assert.Nil(t, user, "we were not expection a user with id 0")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusNotFound, err.StatusCode)
+	assert.EqualValues(t, "not_found", err.Code)
+	assert.EqualValues(t, "user 0 not found", err.Message)
+
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(1)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 1, user.ID)
+	assert.EqualValues(t, "Gerelt-Od", user.FirstName)
+	assert.EqualValues(t, "Surenjav", user.LastName)
+	assert.EqualValues(t, "gereltod.s@gmail.com", user.Email)
+
+}
