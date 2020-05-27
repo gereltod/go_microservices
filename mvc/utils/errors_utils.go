aae04c6fe60a37dd3d7e2dd1a457cc50235605e9
gereltod <gereltod.s@gmail.com>
2020-05-27 12:02:07 +0100

    fix error

--- mvc/utils/errors_utils.go
+++ mvc/utils/errors_utils.go
@@ -2,7 +2,7 @@ package utils
 
 //ApplicationError for http error handle
 type ApplicationError struct {
-	Message string `json:"message`
+	Message string `json:"message"`
 	StatusCode int `json:"status"`
 	Code string `jsong:"code"`
 }
